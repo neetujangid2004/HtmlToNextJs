3e1d271df7451b8cf588d1ed82d60fa6c5c1dcfb
neetujangid2004 <jangidneetu166@gmail.com>
2024-03-16 09:09:13 +0530

    add project

--- Javascript/objects.js
+++ Javascript/objects.js
@@ -2,9 +2,9 @@
 // literals     : not make a singleton
 // constructor  : always make a singleton (one object)  (Object.create)
 
-// object literals 
+console.log("***********   object literals *************"); 
 
-const mySym = Symbol("key1");   // symbol
+const mySym = Symbol("key1");    // symbol
 const mySym1 = Symbol("key2");   // symbol
 
 let jsUser = {
@@ -56,3 +56,64 @@ jsUser1.greeting2 = function(){
     console.log(`Hello js user, ${this.name}`);
 }      
 console.log(jsUser1.greeting2());
+
+
+console.log("*************** Object Constructor ***********");
+const tinderUser = new Object();
+console.log(tinderUser);
+
+const userName = {};
+userName.id = "123abc";
+userName.name = "sammy";
+userName.isLoggedIn = false;
+console.log(userName);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstName: "Neetu",
+            lastName: "Jangid"
+        }
+    }
+}
+console.log(regularUser);
+console.log(regularUser.fullname);
+console.log(regularUser.fullname.userfullname);
+
+console.log("--------------------");
+const obj1 = {
+    1: "a",
+    2: "b"
+};
+const obj2 = {
+    3: "a",
+    4: "b"
+};
+const obj3 = {obj1, obj2 };
+console.log(obj3); 
+
+const obj4 = Object.assign({}, obj1, obj2);     // assign object
+console.log(obj4);
+
+const obj5 = {...obj1, ...obj2};
+console.log(obj5);
+
+const arr = [
+    {
+        id: 1,
+        a: "1"
+    },
+    {
+        id: 1,
+        a: "1"
+    },
+    {
+        id: 1,
+        a: "1"
+    }
+];
+arr[1].id;
+console.log(arr[1].id);
+
+console.log(userName);
